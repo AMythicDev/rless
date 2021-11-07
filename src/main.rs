8cf96bdc2f9fe34cda8134b1bca2f668f129912b
takashiidobe <idobetakashi@gmail.com>
2021-11-06 21:16:26 -0500

    allow less to be piped to a file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use rless::*;
+use atty::Stream;
+use std::fs::read_to_string;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (filename, pager) = arg_parser()?;
-    read_file(filename, pager.finish()).await
+    if atty::is(Stream::Stdout) {
+    	read_file(filename, pager.finish()).await;
+	std::process::exit(0);
+    } 
+	let res = read_to_string(filename);	
+	println!("{}", res.unwrap());
+	Ok(())
 }
